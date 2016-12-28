c28044b475e18f8b2300e1a74515c9119ad4976a
joachimmueller <joachim@wemove.com>
2016-12-28 21:57:34 +0100

    Update app.js
    allow external access

--- app.js
+++ app.js
@@ -40,6 +40,6 @@ app.get('/', routes.index);
 app.post('/', routes.index);
 
 
-http.createServer(app).listen(app.get('port'), 'localhost', function(){
+http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
