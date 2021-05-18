2d5c42d0e3145d618a40a3b9f7e4d30530971319
joachimmueller <jm@wemove.com>
2021-05-18 17:38:38 +0200

    Disable Header "X-Powered-By: Express" for security reason.

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.Router());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.enable('trust proxy');
+app.disable( 'x-powered-by' )
 
 const nedb = new Datastore({filename: 'data/read2burn.db', autoload: true});
 
