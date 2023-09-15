51849d04bc0ff829cd1b12f1934aa54181be7d9a
Dennis <ownerdennis8@gmail.com>
2023-09-15 09:03:57 +0200

    dont save password in DB, URL only

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ exports.index = function (req, res) {
 		const CIPHER_ALGORITHM = 'aes-256-cbc';
 		const ERR_NO_SUCH_ENTRY = 'ERR_NO_SUCH_ENTRY';
 		const PASSWORD_KEY_LENGTH = 32;
+		const UNIQUE_KEY_LENGTH = 8;
 
 		let url = "";
 
@@ -14,29 +15,30 @@ exports.index = function (req, res) {
 			const secretUserMessage = req.body.secretUserMessage;
 			const password = crypto.randomBytes(PASSWORD_KEY_LENGTH).toString('hex');
 			const IV = crypto.randomBytes(16); // Generate a new IV for each encryption
-			const key = generateRandomKey(PASSWORD_KEY_LENGTH);
+			const key = generateRandomKey(UNIQUE_KEY_LENGTH);
 
 
 			let cipher = crypto.createCipheriv(CIPHER_ALGORITHM, Buffer.from(password, 'hex'), IV);
 			let encrypted = cipher.update(secretUserMessage, 'utf8', 'base64') + cipher.final('base64');
 
-			const entry = { key, password, timestamp: Date.now(), encrypted }; // Store only the password
+			const entry = { key, timestamp: Date.now(), encrypted }; // Store only the password
 			nedb.insert(entry, function(err, doc) {
-				url = `https://${req.get('host')}/?key=${key}${IV.toString('hex')}`; // Concatenate password and IV in the URL
+				url = `https://${req.get('host')}/?key=${key}${password}${IV.toString('hex')}`; // Concatenate password and IV in the URL
 				res.render('index', { url: url, secretUserMessage: secretUserMessage, error: undefined, found: false });
 				console.log(`Inserted ${key} with ID ${doc._id}`);
 			});
 		} else if (req.query.key || req.body.key) {
 			let p = req.query.key;
 			if (!p) p = req.body.key;
-			const key = p.substr(0, PASSWORD_KEY_LENGTH * 2); // Hexadecimal representation
-			const IV = p.substr(PASSWORD_KEY_LENGTH * 2);
+			const key = p.substr(0, UNIQUE_KEY_LENGTH * 2); // Hexadecimal representation
+			const password = p.substr(UNIQUE_KEY_LENGTH * 2, PASSWORD_KEY_LENGTH * 2); // Hexadecimal representation
+			const IV = p.substr((PASSWORD_KEY_LENGTH * 2) + (UNIQUE_KEY_LENGTH * 2)); // Hexadecimal representation
 
 			nedb.findOne({ key }, function(err, doc) {
 				try {
 					if (doc.encrypted && req.body.show) {
 						const encrypted = doc.encrypted;
-						let decipher = crypto.createDecipheriv(CIPHER_ALGORITHM, Buffer.from(doc.password, 'hex'), Buffer.from(IV, 'hex'));
+						let decipher = crypto.createDecipheriv(CIPHER_ALGORITHM, Buffer.from(password, 'hex'), Buffer.from(IV, 'hex'));
 						let decrypted = decipher.update(encrypted, 'base64', 'utf8') + decipher.final('utf8');
 
 						nedb.remove({ _id: doc._id }, function(err, numDeleted) {
